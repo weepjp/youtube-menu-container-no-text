d22e777349ad0b840c4a54a3c9318064e8f80aa8
weep <weep@live.jp>
2022-09-30 09:38:59 +0900

    Update ymcnt.user.js

--- ymcnt.user.js
+++ ymcnt.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         youtube-menu-container-no-text
 // @namespace    https://github.com/weepjp/youtube-menu-container-no-text
-// @version      2022.09.30.a
+// @version      2022.09.30.01
 // @description  DetailedTime 使いたいのにテキストが邪魔で見えなくしてくれるからノーテキストにするやつ
 // @author       weepjp https://github.com/weepjp/
 // @license      Released under the GNU General Public License version 3 https://opensource.org/licenses/GPL-3.0
 // @icon         data:image/webp;base64,UklGRmgAAABXRUJQVlA4IFwAAACwAgCdASoQABAAD8D+JaQAOcCzbPsBUndPd3PgoEAAz/pGNqZo2J8hVgUjLGYv70tHddblDC8wq+RSCiA2COJIT2YAWuSZXcTxIGfhVbvPvJl/I1nWxeg9hlAAAA==
 //
-// 
 // @match        https://www.youtube.com/watch?v=*
+// @match        https://www.youtube.com/shorts/*
 //
 // @resource     ymcnt https://raw.githubusercontent.com/weepjp/youtube-menu-container-no-text/main/ymcnt.css
 //
